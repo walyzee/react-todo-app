17aa227777db014dc1950b190e520b49c431e92a
walyzee <61842526+walyzee@users.noreply.github.com>
2020-04-10 17:01:51 +0200

    Update todo.js

--- src/todo.js
+++ src/todo.js
@@ -4,9 +4,7 @@ class Todo extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            items : [],
-            validBtnContent : 'Complete',
-            itemClass : 'text-content'
+            items : []
         }
     }
 
